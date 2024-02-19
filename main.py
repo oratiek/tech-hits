dc36f727ab0843c88f338f36de92145a70b74dfb
Keitaro Miyajima <keitaro.333@studist.jp>
2024-02-19 12:12:31 +0900

    zennの検索ヒット件数を取得する関数を追加

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def qiita(keyword):
     hit_num = int(result.group())
     return hit_num
 
+def zenn(keyword):
+    query = f"https://zenn.dev/search?q={keyword}"
+    res = requests.get(query)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    span = soup.find("span", class_="WithKeywordContent_tabLinkCount__n9jPY")
+    return int(span.text)
+
+
 if __name__ == "__main__":
     qiita_hits = qiita("flask")
     print(qiita_hits)
+    zenn_hits = zenn("flask")
+    print(zenn_hits)
