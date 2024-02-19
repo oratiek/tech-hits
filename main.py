0c22838e11c50c5d043651756de235dc424f3ac5
Keitaro Miyajima <keitaro.333@studist.jp>
2024-02-19 12:29:48 +0900

    stackoverflowの検索ヒット数を取得する関数を追加

--- main.py
+++ main.py
@@ -18,9 +18,22 @@ def zenn(keyword):
     span = soup.find("span", class_="WithKeywordContent_tabLinkCount__n9jPY")
     return int(span.text)
 
+def stackoverflow(keyword):
+    query = f"https://stackoverflow.com/questions/tagged/{keyword}"
+    res = requests.get(query)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    div = soup.find("div", class_="fs-body3 flex--item fl1 mr12 sm:mr0 sm:mb12")
+    result = re.search(r'.*\d+', div.text)
+    hit_num = result.group()
+    if "," in hit_num:
+        comma_index = hit_num.find(",")
+        hit_num = hit_num[0:comma_index] + hit_num[comma_index+1:]
+    return int(hit_num)
 
 if __name__ == "__main__":
     qiita_hits = qiita("flask")
     print(qiita_hits)
     zenn_hits = zenn("flask")
     print(zenn_hits)
+    sof = stackoverflow("flask")
+    print(sof)
